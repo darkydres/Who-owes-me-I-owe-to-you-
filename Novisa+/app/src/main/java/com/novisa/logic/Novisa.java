a7f367dbbdddea96842c2f70acb292d27685865e
darkydres <teradres@hotmail.com>
2015-08-19 01:51:08 -0500

    First commit

--- Novisa+/app/src/main/java/com/novisa/logic/Novisa.java
+++ Novisa+/app/src/main/java/com/novisa/logic/Novisa.java
@@ -20,5 +20,28 @@ public class Novisa {
         managers = new ArrayList<ManagerAccount>();
     }
 
+    public boolean existClient(Client client)
+    {
+        boolean exist = false;
+
+            if(clients.contains(client))
+            {
+                exist = true;
+            }
+        return exist;
+    }
+
+    public boolean addNewClient(String clientName, int id, String cellphone, String address, String email)
+    {
+        boolean wasRight = false;
+        Client newClient = new Client(clientName, id, cellphone, address, email);
+        if(existClient(newClient) == false)
+        {
+            clients.add(newClient);
+            wasRight = true;
+        }
+
+        return wasRight;
+    }
 
 }
